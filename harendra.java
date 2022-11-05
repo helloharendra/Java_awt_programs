9aa659114cd13c7152c9c406f9622d5c54cab3bc
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-11-05 09:58:29 +0530

    othert change

--- harendra.java
+++ harendra.java
@@ -1,15 +1,15 @@
 import java.awt.*;
 import java.awt.event.*;
-class newJavaButton extends Frame implements ActionListener{
+class harendra extends Frame implements ActionListener{
     TextField tf;
-    newJavaButton(){
+    harendra(){
     Label l=new Label("It will change Dynamically"); 
     l.setBounds(165,30,200,20);  
     tf=new TextField();
     tf.setBounds(165,60,200,20);// here 165 is x axis, 60 is y axis,100 is width and 20 is height of text field
     Button b=new Button("Submit");
     b.setBounds(220,120,80,30);
-    
+
     b.addActionListener(this);
     add(l);
     add(b);
@@ -23,7 +23,7 @@ class newJavaButton extends Frame implements ActionListener{
     tf.setText("hello harendra");
    }
    public static void main(String[] args) {
-   new newJavaButton();
+   new harendra();
    }
 
     
